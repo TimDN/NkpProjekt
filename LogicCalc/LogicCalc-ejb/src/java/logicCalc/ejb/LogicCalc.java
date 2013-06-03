357afb73449f81ec3e5aa0d1d2718921a3085ec9
Tim Nielsen <tim.daldorph.nielsen@gmail.com>
2013-06-03 15:34:31 +0200

    Test client added
    Added a client,changed the methods,bugfixes

--- LogicCalc/LogicCalc-ejb/src/java/logicCalc/ejb/LogicCalc.java
+++ LogicCalc/LogicCalc-ejb/src/java/logicCalc/ejb/LogicCalc.java
@@ -16,17 +16,63 @@ import javax.ejb.LocalBean;
 public class LogicCalc {
     
 public static Calculator calc;
-
+    //Anropar kalkylatorn svaret från den är en sträng med svaret
     public String calculate(String inString)
     {
-        calc = new Calculator(inString);
+        String outString = new String();
+        String errors = null;
         
-        String outString = calc.startCalc();
+        errors = finalErrorCheck(inString);
+        if(errors == null)
+        {
+            calc = new Calculator(inString);
+            outString = calc.startCalc();
+        }
+        else
+        {
+          outString = errors;  
+        }
+        
         return outString;
     }
-    public String errorCheck(String inString)
+    //Kollar igenom hela strängen efter fel
+    public String finalErrorCheck(String checkString)
     {
-        String outString = this.calculate(inString);
-        return outString;
+        String errorCheckResult = null;
+        
+        for(int i=0 ; i<(checkString.length()-1) ; i++)
+        {
+            if(checkString.charAt(i) == checkString.charAt(i+1))
+            {
+                errorCheckResult += "You have placed a \"1\" / \"0\" / \"|\" / \"&\" after each other\n";
+            }
+        }
+        //kollar om ^ ,|  eller & är felplacerade
+        if(checkString.indexOf("(&")>0 || checkString.indexOf("&)")>0 || checkString.indexOf("(|")>0 || checkString.indexOf("|)")>0 || 
+                checkString.indexOf("^(")>0 || checkString.indexOf("^)")>0)
+        {
+            errorCheckResult += "You have placed a \"|\" , \"&\" or a \"^\" before or after a bracket\n";
+        }
+        else if(checkString.indexOf("1(")>0 || checkString.indexOf("0(")>0 || checkString.indexOf(")1")>0 || 
+                checkString.indexOf(")0")>0 || checkString.startsWith("1(") || checkString.startsWith("0("))
+        {
+            errorCheckResult += "You have placed a \"1\" or a \"0\" before or after a bracket\n";
+        }
+        //kollar om NOT(!) ligger rätt
+        else if(checkString.indexOf("!^")>0 || checkString.indexOf("!&")>0 || checkString.indexOf("!|")>0 || checkString.indexOf("!)")>0)
+        {
+            errorCheckResult += "You have placed \"!\" in a wrong position";
+        }
+        //kollar om kalkylatorn startar med fel tecken
+        else if(checkString.startsWith("|") || checkString.startsWith("&") || checkString.startsWith(")") || checkString.startsWith("^"))
+        {
+            errorCheckResult += "You have to start the calculation with a digit, \"(\" or a \"!\"";
+        }
+        
+        return errorCheckResult;
+    }
+    public String errorCheck(String checkString)
+    {
+        return "";
     }
 }
